4e83d463bb7cf5d50f3febc99a423cd888c8e3fb
Chris Fiorino <chris.fiorino@gmail.com>
2026-03-27 09:50:04 -0400

    Fix white overscroll background on iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-gray-950`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
